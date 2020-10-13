a827a636eeee22308429f3adc546ae5f6249d42e
Sergey Obukhov <osergey@live.com>
2020-10-13 22:39:53 +0300

    Format metrics output

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,41 @@
 #include <iostream>
 using namespace packpars;
 
+class PrintMetricsUtil {
+	std::list<Metric> metrics_;
+	size_t maxDescriptionSize() const {
+		size_t mdesc;
+		for(const Metric& metric : metrics_) {
+			if(metric.description.size() > mdesc) {
+				mdesc = metric.description.size();
+			}
+		}
+		return mdesc;
+	}
+public:
+	explicit PrintMetricsUtil(const std::list<Metric>& metrics) :
+		metrics_(metrics) {
+		metrics_.sort();
+	}
+	PrintMetricsUtil(const PrintMetricsUtil&) = delete;
+	PrintMetricsUtil& operator=(const PrintMetricsUtil&) = delete;
+	void print() const {
+		size_t const mdesc = maxDescriptionSize();
+		for(const Metric& metric : metrics_) {
+			std::cout.width(mdesc);
+			std::cout.setf(std::cout.flags() | std::ios::left);
+			std::cout << metric.description << " : " << metric.value << std::endl;
+		}
+	}
+};
+
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
 		std::cerr << "Usage: packpars <directory>";
 		return 0;
 	}
 	try {
-		std::list<Metric> metrics = Processor(argv[1]).process();
-		metrics.sort();
-		for(const Metric& metric : metrics) {
-			std::cout << metric.description << ": " << metric.value << std::endl;
-		}
+		PrintMetricsUtil(Processor(argv[1]).process()).print();
 	}
 	catch(const std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
