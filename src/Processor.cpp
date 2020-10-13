a716d1cdf08d8a1361ee8d9bc0cb02e3b178ad89
Sergey Obukhov <osergey@live.com>
2020-10-13 22:15:57 +0300

    Error handling

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -1,15 +1,35 @@
 #include "Processor.h"
 #include "Parser.h"
 #include <pcap.h>
+#include <exception>
 
 namespace packpars {
+
+class PcapCallChecker {
+public:
+    PcapCallChecker() = delete;
+    PcapCallChecker(const PcapCallChecker&) = delete;
+    PcapCallChecker& operator=(const PcapCallChecker&) = delete;
+    static pcap_t* open(std::string_view filename) {
+        char errbuf[PCAP_ERRBUF_SIZE];
+        if(auto pcap = pcap_open_offline(filename.data(), errbuf)) {
+            return pcap;
+        }
+        throw std::runtime_error(errbuf);
+    }
+    static void loop(int error) {
+        if(error != 0) {
+            throw std::runtime_error("pcap loop error");
+        }
+    }
+};
 
 void processorCallback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
     reinterpret_cast<Parser*>(args)->process(packet, pkthdr->len);
 }
 
 Processor::Processor(std::string_view filename) :
-    pcap_(pcap_open_offline(filename.data(), nullptr)) {}
+    pcap_(PcapCallChecker::open(filename)) {}
 
 Processor::~Processor() {
     try {
@@ -22,7 +42,7 @@ Processor::~Processor() {
 
 std::list<Metric> Processor::process() {
     std::unique_ptr<Parser> common = Parser::common();
-    pcap_loop(pcap_, -1, processorCallback, reinterpret_cast<u_char*>(common.get()));
+    PcapCallChecker::loop(pcap_loop(pcap_, -1, processorCallback, reinterpret_cast<u_char*>(common.get())));
     std::list<Metric> metrics;
     common->metrics(metrics);
     return metrics;
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,24 @@
 using namespace packpars;
 
 int main(int argc, char* argv[]) {
-	if(argc > 1) {
+	if(argc < 2) {
+		std::cerr << "Usage: packpars <directory>";
+		return 0;
+	}
+	try {
 		std::list<Metric> metrics = Processor(argv[1]).process();
 		metrics.sort();
 		for(const Metric& metric : metrics) {
 			std::cout << metric.description << ": " << metric.value << std::endl;
 		}
+	}
+	catch(const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return -1;
+	}
+	catch(...) {
+		std::cerr << "Error: unknown" << std::endl;
+		return -1;
 	}
 	return 0;
 }
